70a9d5d0a131769ccd5652f48ca33a349ea81c75
Ethan Black <39673044+golemwire@users.noreply.github.com>
2020-11-27 20:27:52 -0500

    Update TTLaserEngraver.ino

--- TTLaserEngraver.ino
+++ TTLaserEngraver.ino
@@ -1,5 +1,12 @@
+#include <PS2Keyboard.h>         //Keyboard
+#include <SPI.h>                 //DACs
+
+// LCD over I²C:
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+
+// Keyboard:
+PS2Keyboard keyboard;
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
