e4aa8eda026f60828d0b388a6b0aa3d0def5dbd9
Ethan Black <39673044+golemwire@users.noreply.github.com>
2020-12-05 20:19:41 -0500

    Corrected comment

--- TTLaserEngraver.ino
+++ TTLaserEngraver.ino
@@ -8,7 +8,7 @@
 // Keyboard:
 PS2Keyboard keyboard;
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 20 chars and 4 line display
 
 void setup() {
     lcd.init();                      // initialize the lcd
